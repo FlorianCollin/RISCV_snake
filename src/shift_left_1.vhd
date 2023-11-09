06c79e129f159060c4b0c96ed6b64314244aadc0
Collin <flocol1302@gmail.com>
2023-11-09 22:55:48 +0100

    add shift_left_1

---
+++ src/shift_left_1.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+use work.constants_pkg.all;
+
+entity shift_led_1 is
+    Port ( 
+        d  : in  std_logic_vector(DATA_LENGTH - 1 downto 0);
+        q : out std_logic_vector(DATA_LENGTH - 1 downto 0)
+    );
+end shift_led_1;
+
+architecture Behavioral of shift_led_1 is
+begin
+    process (d)
+    begin
+        q <= '0' & d(DATA_LENGTH - 1 downto 1);
+    end process;
+end Behavioral;
